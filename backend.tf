62544aaec60a50ae08463c1ca6316b6753e1e05d
visaluser <132979509+visaluser@users.noreply.github.com>
2023-12-28 15:38:01 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
         bucket = "bucket-0011-erg123"
         key     = "myproject02/terraform.tfstate"
-        region = "us-west-1"
+        region = "us-west-2"
   }
 }
