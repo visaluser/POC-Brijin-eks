a8e482d616be11700169d53fb05710bfa02677ac
visaluser <132979509+visaluser@users.noreply.github.com>
2024-01-02 15:39:08 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-        bucket = "sanj-back-1212"
+        bucket = "vis-back-1212"
         key     = "myproject022/terraform.tfstate"
         region = "us-east-2"
   }
